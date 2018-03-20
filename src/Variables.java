b800b3cf27e6ee1baf3a1d1682c49374da4ab7c0
Sean Rickards <seanrickards56@gmail.com>
2018-03-20 08:07:51 +0000

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        byte userAge = 20;
+        short numberOfStudents = 45;
+        int numberOfEmployees = 500;
+        long numberOfInhabitants = 21021313012678L;
+
+        float hourlyRate = 60.5F ;
+        double numberOfHours = 5120.5;
+
+        char grade = 'A';
+        boolean promote = true;
+
+        byte level = 2, userExperiance = 5;
+
+        String test = "Test";
+
+        System.out.println( "User Age" + " " + userAge );
+    }
+
+}
