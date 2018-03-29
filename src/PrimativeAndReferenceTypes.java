c00e88e754aa4a410ab170fca21091c89c6001e4
Sean Rickards <seanrickards56@gmail.com>
2018-03-29 11:56:36 +0100

    Finished Chapter 4

--- src/PrimativeAndReferenceTypes.java
+++ src/PrimativeAndReferenceTypes.java
@@ -3,7 +3,11 @@ public class PrimativeAndReferenceTypes {
     public static void main(String[] args) {
         int myNumber = 5; // Variable myNumber stored the actual number 5
 
-        String message = "Hello"; // Variable message doesnt store the string "Hello"
+        String message = "Hello"; // Variable message doesn't store the string "Hello" instead it is held in memory. the variable message stores the location of string message.
+
+        message = "World";
+
+        System.out.println(message);
     }
 
 }
