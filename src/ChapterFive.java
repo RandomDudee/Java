91d58665aeb17255e4863af9e4209535d722bdab
Sean Rickards <seanrickards56@gmail.com>
2018-04-25 15:39:49 +0100

    Continuing Chapter 5

--- src/ChapterFive.java
+++ src/ChapterFive.java
@@ -14,15 +14,29 @@ public class ChapterFive {
 
         System.out.println("Oracle".substring(1, 4)); // Method in println
 
-        // Escape Sequences
-
+        // 5.2 Escape Sequences
         // \t = tab. \n = newline. \\ = \. \" = ".
 
-        //Formatting Outputs
-
+        // 5.3 Formatting Outputs
         System.out.println("The answer for 5.45 divided by 3 is " + (5.45/3));
 
         System.out.printf("The Answer for %.3f divided by %d is %.2f.",5.45, 3, 3.45/3);
+
+        System.out.println("\n");
+
+        System.out.println(30 + 5);
+
+        System.out.println("Hello, " + "how are you?" + " I love Java. ");
+
+        int results = 79;
+        System.out.println("You scored " + results + " marks for your test.");
+
+        System.out.println("The sum of 50 and 2 is " + (50 + 2) + ".");
+
+        // 5.3.1 Converters
+
+
+
 
     }
 
