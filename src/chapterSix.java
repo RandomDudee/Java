f6cadce9bf36ae299e72c6677fb2c4c93d9347de
Sean Rickards <seanrickards56@gmail.com>
2018-04-26 14:48:44 +0100

    First If Statement

---
+++ src/chapterSix.java
@@ -0,0 +1,38 @@
+public class chapterSix {
+    public static void main(String[] args) {
+        //Control Flow Statements
+        /*
+        Decision-making(if, switch)
+        Looping (for, while, do-while)
+        Branching (break, continue)
+         */
+
+        // 6.1 Comparison Operators
+        /*
+        Equals to ==
+        Not equal !=
+        Greater than / equal to > / >=
+        Smaller than / equal to < / <=
+        AND operator &&
+        OR operator ||
+        */
+
+        // 6.2 Decision making statements
+        // 6.2.1 If Statements
+
+//        if (condition 1 is met){
+//            do Task A
+//        }
+//        else if (condition 2 is met){
+//            do Task B
+//        }
+//        else if (condition 3 is met){
+//            do Task C
+//        }
+//        else{
+//            do task D
+//        }
+
+
+    }
+}
