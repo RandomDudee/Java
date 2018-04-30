5072b7c55e25e7ac9bdab53bef39d957979e5ff3
Sean Rickards <seanrickards56@gmail.com>
2018-04-30 15:55:51 +0100

    Starting Chapter Seven

--- src/chapterSeven.java
+++ src/chapterSeven.java
@@ -2,7 +2,14 @@ public class chapterSeven {
 
     public static void main(String[] args) {
 
-
+        // Chapter 7: Object Oriented Programming Part 1
+        // 7.2 Writing our own class
+        /*
+        AccessModifier class ClassName {
+            Contents of the class
+            Including fields, constructors and methods
+        }
+         */
 
     }
 }
