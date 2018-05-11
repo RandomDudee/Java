ddc9d1c752ce84740d75390d892957924b014800
Sean Rickards <seanrickards56@gmail.com>
2018-05-11 09:37:54 +0100

    8.5 Complete

---
+++ ModifierDemo/src/anotherPackage/ClassesInAnotherPAckage.java
@@ -0,0 +1,16 @@
+package anotherPackage;
+
+public class ClassesInAnotherPAckage {
+
+    //just an empty class
+}
+
+class MyClassC extends ModifierDemo{
+    public void printMessages()
+    {
+
+        //This is okay
+        System.out.println(publicNum);
+    }
+
+}
