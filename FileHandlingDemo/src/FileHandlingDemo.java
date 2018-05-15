b793831ace83d616c755049476d71eabdae26138
Sean Rickards <seanrickards56@gmail.com>
2018-05-15 14:08:44 +0100

    10.1 FileHandlingDemo Complete

--- FileHandlingDemo/src/FileHandlingDemo.java
+++ FileHandlingDemo/src/FileHandlingDemo.java
@@ -4,16 +4,12 @@ import java.io.*;
 public class FileHandlingDemo {
 
     public static void main(String[] args) {
-
         String line;
-        BufferedReader reader = null;
 
-        try
+        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.rtf")))
         {
-            reader = new BufferedReader(new FileReader("myFile.txt"));
             line = reader.readLine();
-            while (line != null)
-            {
+            while (line != null){
                 System.out.println(line);
                 line = reader.readLine();
             }
@@ -22,17 +18,37 @@ public class FileHandlingDemo {
         {
             System.out.println(e.getMessage());
         }
-        finally
-        {
-            try
-            {
-                if (reader != null)
-                    reader.close();
-            }
-            catch (IOException e)
-            {
-                System.out.println(e.getMessage());
-            }
-        }
+
     }
+
+//        String line;
+//        BufferedReader reader = null;
+//
+//        try
+//        {
+//            reader = new BufferedReader(new FileReader("myFile.rtf"));
+//            line = reader.readLine();
+//            while (line != null)
+//            {
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
+//        }
+//        catch (IOException e)
+//        {
+//            System.out.println(e.getMessage());
+//        }
+//        finally
+//        {
+//            try
+//            {
+//                if (reader != null)
+//                    reader.close();
+//            }
+//            catch (IOException e)
+//            {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+//    }
 }
