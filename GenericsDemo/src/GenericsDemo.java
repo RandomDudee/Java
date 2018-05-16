44bbb8893761e6152dcc60af71b44fd9dbdfe3aa
Sean Rickards <seanrickards56@gmail.com>
2018-05-16 11:36:04 +0100

    Chapter 11

---
+++ GenericsDemo/src/GenericsDemo.java
@@ -0,0 +1,23 @@
+public class GenericsDemo {
+
+    public static void main(String[] args) {
+
+        MyGenericsClass<Integer> g = new MyGenericsClass();
+
+        g.setMyVar(10);
+        g.printValue();
+    }
+}
+
+class MyGenericsClass<T>{
+
+    private T myVar;
+
+    void setMyVar (T i){
+        myVar = i;
+    }
+
+    void printValue(){
+        System.out.println("The value of myVar is " + myVar);
+    }
+}
