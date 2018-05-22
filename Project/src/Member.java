7f951b9029c3364e30a9aff31f19e2077e50c26f
Sean Rickards <seanrickards56@gmail.com>
2018-05-22 14:20:27 +0100

    Continuing the Project

--- Project/src/Member.java
+++ Project/src/Member.java
@@ -1,2 +1,18 @@
 public class Member {
+
+    private char memberType;
+    private int memberID;
+    private String name;
+    private double fees;
+
+    public Member(char pMemberType, int pMemberID, String pName, double pFees)
+    {
+        memberType = pMemberType;
+        memberID = pMemberID;
+        name = pName;
+        fees = pFees;
+    }
+
+
+
 }
